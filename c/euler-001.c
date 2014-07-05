ace3e42173b83695427bf8882373d746d4b0c6bd
Nino Ninux <nino.ninux@gmail.com>
2014-07-06 00:44:34 +0200

    solved problem 1 in c

---
+++ c/euler-001.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int range = 1000;
+	int result = 0;
+	int i = 0;
+
+	for(i = 0; i < range; i++) {
+		if (i%3 == 0) {
+			result += i;
+		}
+		else {
+			if (i%5 == 0) {
+				result += i;
+			}
+		}
+	}
+
+	printf("%i\n", result);
+}
+
+
